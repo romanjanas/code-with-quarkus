7fc489807cb7805b56c0d9d303ee1627322003d5
Roman <janas.roman@gmail.com>
2025-05-19 23:13:17 +0200

    added grreting endpoint with name

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,30 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+import static jakarta.ws.rs.core.MediaType.*;
+
+@Path("/")
 public class GreetingResource {
 
+    @Inject
+    private GreetingService greetingService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(TEXT_PLAIN)
+    @Path("/hello")
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Produces(TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return greetingService.greeting(name);
     }
 }
